6916dc0d0a7be5b08295147eda5e11b17b986e6c
Joe Muraguri <joe.crrm@gmail.com>
2022-04-28 19:09:05 +0300

    second commit

--- index.php
+++ index.php
@@ -10,18 +10,19 @@ if ($text == " ") {
     $response = "CON What do you want to check \n";
     $response .= "1.My Dairy Account No.\n";
     $response .= "2. My Phone Number \n";
-} else if($text == "1"){
+} else if ($text == "1") {
     $response = "CON Choose account information to view \n";
     $response .= "1. Account Number \n";
     $response .= "2. Account Balance \n";
-}else if($text == "2"){
-    $response = "END Your phone Number is".$phoneNumber;
-}else if($text == "1*1"){
+} else if ($text == "2") {
+    $response = "END Your phone Number is" . $phoneNumber;
+} else if ($text == "1*1") {
     $accountNumber = "ACC001";
-    $response = "END Your account Number is ".$accountNumber;
-}else if($text == "1*2"){
+    $response = "END Your account Number is " . $accountNumber;
+} else if ($text == "1*2") {
     $balance = "KSH 10,000";
-    $response = "END Your balance is".$balance;
+    $response = "END Your balance is" . $balance;
+
+    echo $response;
 }
 header('Content-type;text/plain');
-echo $response;
